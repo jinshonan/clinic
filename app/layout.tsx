9cfc9dfee985f23dfaf34b445bef3381e6a5b518
Jin Shonan <jeffrey.zsn1126@gmail.com>
2025-06-13 01:46:37 +0900

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import { Plus_Jakarta_Sans } from "next/font/google";
+import "./globals.css";
+
+import { cn } from '@/lib/utils'
+import { ThemeProvider } from "@/components/theme-provider";
+
+const fontSans = Plus_Jakarta_Sans({
+  variable: "--font-sans",
+  subsets: ["latin"],
+  weight: ['300', '400', '500', '600', '700'],
+});
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+export const metadata: Metadata = {
+  title: "クリンニック",  // CarePulse
+  description: "A healthcare management system",  // A healthcare management system
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.variable)}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"  // always use the dark mode
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+    </html>
+  );
+}
