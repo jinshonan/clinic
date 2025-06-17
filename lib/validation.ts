ab573bfdd29506fa633f22d8f84a4134c89b3c76
Jin Shonan <jeffrey.zsn1126@gmail.com>
2025-06-17 23:54:05 +0900

    appwrite fixed

--- lib/validation.ts
+++ lib/validation.ts
@@ -5,5 +5,7 @@ export　const UserFormValidation = z.object({
         .min(2, "２文字以上")
         .max(10, "10文字以下"),
     email: z.string().email("正しいメールを入力してください"),
-    phone: z.string().refine((phone) => /^\+\d{10, 15}$/.test(phone), "正しい電話番号を入力してください")
+    phone: z.string()
+    // the validation kills me
+    // phone: z.string().refine((phone) => /^\+\d{10, 15}$/.test(phone), "正しい電話番号を入力してください")
 })
